06109c387a10bc8311a0d99cd77b21e61361fbde
Jin Kwon <onacit@gmail.com>
2026-01-05 10:37:43 +0900

    wip

--- src/main/java/io/github/jinahya/rickandmortyapi/spring/web/bind/LocationResidentsController.java
+++ src/main/java/io/github/jinahya/rickandmortyapi/spring/web/bind/LocationResidentsController.java
@@ -6,10 +6,13 @@ import io.github.jinahya.rickandmortyapi.persistence.LocationResident_;
 import io.github.jinahya.rickandmortyapi.spring.stereotype.LocationResidentService;
 import io.github.jinahya.rickandmortyapi.spring.web.bind.type.LocationResidentType;
 import io.github.jinahya.rickandmortyapi.spring.web.bind.type.mapper.LocationResidentTypeMapper;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.enums.ParameterIn;
+import io.swagger.v3.oas.annotations.enums.ParameterStyle;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.criteria.JoinType;
+import jakarta.validation.constraints.Positive;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import org.jspecify.annotations.Nullable;
@@ -23,11 +26,11 @@ import org.springframework.hateoas.Link;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.hateoas.PagedModel;
 import org.springframework.http.HttpStatus;
-import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.MatrixVariable;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
@@ -98,9 +101,9 @@ class LocationResidentsController {
                             .withRel(LocationResidentType.RELATION_LOCATION)
             );
         });
-        Optional.ofNullable(content.getResident()).ifPresent(l -> {
+        Optional.ofNullable(content.getResident()).ifPresent(r -> {
             entityModel.add(
-                    linkTo(methodOn(LocationsController.class).readLocation(l.getId()))
+                    linkTo(methodOn(CharactersController.class).readSingle(r.getId()))
                             .withRel(LocationResidentType.RELATION_RESIDENT)
             );
         });
@@ -134,35 +137,52 @@ class LocationResidentsController {
     PagedModel<EntityModel<LocationResidentType>> read(
 
             @Parameter(required = false,
-                       schema = @Schema(type = "string",
-                                        defaultValue = REQUEST_MAPPING_PATH_VALUE
-                       ),
+                       schema = @Schema(type = "string", defaultValue = REQUEST_MAPPING_PATH_VALUE),
                        in = ParameterIn.PATH
             )
             @DefaultValue(REQUEST_MAPPING_PATH_VALUE)
-            @PathVariable(name = REQUEST_MAPPING_PATH_NAME, required = false) final String requestMappingPath,
+            @PathVariable(name = REQUEST_MAPPING_PATH_NAME,
+                          required = false
+            ) final String requestMappingPath,
+
+            @Positive
+            @RequestParam(name = PARAM_NAME_ID_LOCATION_ID, required = false) // just for the UI
+            final Integer idLocationIdParam,
 
             @Parameter(required = false,
-                       schema = @Schema(type = "integer", format = "int32"),
-//                       in = ParameterIn.PATH,
+                       schema = @Schema(type = "integer", format = "int32", example = ";id.locationId=1"),
+                       in = ParameterIn.PATH,
+                       style = ParameterStyle.MATRIX,
                        description = "id.locationId to filter"
             )
+            @Positive
             @MatrixVariable(pathVar = REQUEST_MAPPING_PATH_NAME,
                             name = PARAM_NAME_ID_LOCATION_ID,
                             required = false
-            ) final Integer idLocationId,
+            ) final Integer idLocationIdVariable,
+
+            @Positive
+            @RequestParam(name = PARAM_NAME_ID_RESIDENT_ID, required = false) // just for the UI
+            final Integer idResidentIdParam,
 
             @Parameter(required = false,
-                       schema = @Schema(type = "integer", format = "int32"),
-//                       in = ParameterIn.PATH,
+                       schema = @Schema(type = "integer", format = "int32", example = ";id.residentId=1"),
+                       in = ParameterIn.PATH,
+                       style = ParameterStyle.MATRIX,
                        description = "id.residentId to filter"
             )
+            @Positive
             @MatrixVariable(pathVar = REQUEST_MAPPING_PATH_NAME,
                             name = PARAM_NAME_ID_RESIDENT_ID,
-                            required = false) final Integer idResidentId,
+                            required = false
+            ) final Integer idResidentIdVariable,
 
             @ParameterObject final Pageable pageable) {
-        final var found = locationResidentService.applyRepository(repo -> repo.findAll(
+
+        final var idLocationId = Optional.ofNullable(idLocationIdVariable).orElse(idLocationIdParam);
+        final var idResidentId = Optional.ofNullable(idResidentIdVariable).orElse(idResidentIdParam);
+
+        final var selected = locationResidentService.applyRepository(repo -> repo.findAll(
                 (r, q, b) -> {
                     if (q.getResultType() != Long.class) {
                         r.fetch(LocationResident_.LOCATION, JoinType.LEFT);
@@ -180,16 +200,34 @@ class LocationResidentsController {
                                             r.get(LocationResident_.ID).get(LocationResidentId_.RESIDENT_ID),
                                             v
                                     ))
-                                    .orElseGet(b::conjunction));
+                                    .orElseGet(b::conjunction)
+                    );
                 },
                 pageable
         ));
-        final var mapped = found.map(locationResidentTypeMapper::fromEntity);
-        final var assembled = pagedResourcesAssembler.toModel(mapped);
-        final var decorated = decorate(assembled);
+        final var mapped = selected.map(locationResidentTypeMapper::fromEntity);
+        final var modeled = pagedResourcesAssembler.toModel(mapped);
+        final var decorated = decorate(modeled);
         return decorated;
     }
 
+    @Operation(
+            summary = "Reads a locationResident.",
+            parameters = {
+                    @Parameter(
+                            name = PARAM_NAME_ID_LOCATION_ID,
+                            in = ParameterIn.PATH,
+                            style = ParameterStyle.MATRIX,
+                            schema = @Schema(type = "integer", format = "int32")
+                    ),
+                    @Parameter(
+                            name = PARAM_NAME_ID_RESIDENT_ID,
+                            in = ParameterIn.PATH,
+                            style = ParameterStyle.MATRIX,
+                            schema = @Schema(type = "integer", format = "int32")
+                    ),
+            }
+    )
     @GetMapping(
             path = {
                     REQUEST_MAPPING_PATH_TEMPLATE + '/' + PATH_TEMPLATE_DONTCARE
@@ -199,29 +237,80 @@ class LocationResidentsController {
             }
     )
     EntityModel<LocationResidentType> readSingle(
+
+            @Parameter(required = false,
+                       schema = @Schema(type = "string", defaultValue = REQUEST_MAPPING_PATH_VALUE),
+                       in = ParameterIn.PATH
+            )
+            @DefaultValue(REQUEST_MAPPING_PATH_VALUE)
             @PathVariable(name = REQUEST_MAPPING_PATH_NAME) final String requestMappingPath,
-            @MatrixVariable(pathVar = REQUEST_MAPPING_PATH_NAME) final MultiValueMap<String, String> matrixVariables,
+
+//            @ParameterObject
+////            @Parameter(hidden = true)
+//            @MatrixVariable(pathVar = REQUEST_MAPPING_PATH_NAME) final MultiValueMap<String, String> matrixVariables,
+
+            @Parameter(required = false,
+                       schema = @Schema(type = "integer", format = "int32", example = ";id.locationId=1"),
+                       in = ParameterIn.PATH,
+                       style = ParameterStyle.MATRIX,
+                       description = "id.locationId to filter"
+            )
+            @Positive
+            @MatrixVariable(pathVar = REQUEST_MAPPING_PATH_NAME,
+                            name = PARAM_NAME_ID_LOCATION_ID,
+                            required = false
+            ) final Integer idLocationId,
+
+            @Parameter(required = false,
+                       schema = @Schema(type = "integer", format = "int32", example = ";id.residentId=1"),
+                       in = ParameterIn.PATH,
+                       style = ParameterStyle.MATRIX,
+                       description = "id.residentId to filter"
+            )
+            @Positive
+            @MatrixVariable(pathVar = REQUEST_MAPPING_PATH_NAME,
+                            name = PARAM_NAME_ID_RESIDENT_ID,
+                            required = false
+            ) final Integer idResidentId,
+
+            @Parameter(required = false,
+                       schema = @Schema(type = "string", defaultValue = PATH_VALUE_DONTCARE),
+                       in = ParameterIn.PATH
+            )
+            @DefaultValue(PATH_VALUE_DONTCARE)
             @PathVariable(name = PATH_NAME_DONTCARE) final String dontcare) {
-        final var idLocationId =
-                Optional.ofNullable(matrixVariables.getFirst(PARAM_NAME_ID_LOCATION_ID))
-                        .orElseThrow(() -> new ResponseStatusException(
-                                HttpStatus.BAD_REQUEST,
-                                PARAM_NAME_ID_LOCATION_ID + " is required"
-                        ));
-        final var idResidentId =
-                Optional.ofNullable(matrixVariables.getFirst(PARAM_NAME_ID_RESIDENT_ID))
-                        .orElseThrow(() -> new ResponseStatusException(
-                                HttpStatus.BAD_REQUEST,
-                                PARAM_NAME_ID_RESIDENT_ID + " is required"
-                        ));
-        final var id = LocationResidentId.of(
-                Integer.valueOf(idLocationId), // NumberFormatException
-                Integer.valueOf(idResidentId)  // NumberFormatException
-        );
-        final var found = locationResidentService
+
+//        final var idLocationId =
+//                Optional.ofNullable(matrixVariables.getFirst(PARAM_NAME_ID_LOCATION_ID))
+//                        .map(v -> {
+//                            try {
+//                                return Integer.valueOf(v);
+//                            } catch (final NumberFormatException nfe) {
+//                                throw new ResponseStatusException(HttpStatus.BAD_REQUEST, nfe.getMessage(), nfe);
+//                            }
+//                        })
+//                        .orElseThrow(() -> new ResponseStatusException(
+//                                HttpStatus.BAD_REQUEST,
+//                                PARAM_NAME_ID_LOCATION_ID + " is required"
+//                        ));
+//        final var idResidentId =
+//                Optional.ofNullable(matrixVariables.getFirst(PARAM_NAME_ID_RESIDENT_ID))
+//                        .map(v -> {
+//                            try {
+//                                return Integer.valueOf(v);
+//                            } catch (final NumberFormatException nfe) {
+//                                throw new ResponseStatusException(HttpStatus.BAD_REQUEST, nfe.getMessage(), nfe);
+//                            }
+//                        })
+//                        .orElseThrow(() -> new ResponseStatusException(
+//                                HttpStatus.BAD_REQUEST,
+//                                PARAM_NAME_ID_RESIDENT_ID + " is required"
+//                        ));
+        final var id = LocationResidentId.of(idLocationId, idResidentId);
+        final var selected = locationResidentService
                 .applyRepository(repo -> repo.findById(id))
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
-        final var mapped = locationResidentTypeMapper.fromEntity(found);
+        final var mapped = locationResidentTypeMapper.fromEntity(selected);
         final var modeled = EntityModel.of(mapped);
         final var decorated = decorate(modeled);
         return decorated;
